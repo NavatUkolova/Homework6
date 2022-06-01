c325704fa2eb42bb5631e54739e18c8211e728ae
Ukolova Nabat <ru.nabat@gmail.com>
2022-06-01 20:07:30 +0400

    zadacha03

---
+++ zadacha03/Program.cs
@@ -0,0 +1,39 @@
+﻿//В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+void FillArray(int[,] matrix)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = random.Next(0, 10);
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+void SquareOfElementWithEvenIndexes(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            if (i % 2 == 0 && j % 2 == 0) 
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите кол-во столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+Console.WriteLine("Начальный массив: ");
+PrintArray(matrix);
+Console.WriteLine("Массив с квадратами элементов с четными индексами: ");
+SquareOfElementWithEvenIndexes(matrix);
+PrintArray(matrix);
